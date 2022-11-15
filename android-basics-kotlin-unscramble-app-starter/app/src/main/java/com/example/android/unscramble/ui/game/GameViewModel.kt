5b470590a31b8517ff8eec07a1038d670a5c5c09
Freya0119 <103048480+Freya0119@users.noreply.github.com>
2022-11-15 10:47:47 +0900

    9. Implement OnClickListener for Submit button
    Update the text field
    In onSubmitWord()

--- android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,21 +5,22 @@ import androidx.lifecycle.ViewModel
 import androidx.fragment.app.viewModels
 
 class GameViewModel : ViewModel() {
+    private var _score = 0
+    var currentWordCount = 0
+    private var _currentScrambledWord: String = "test"
+
+    val currentScrambledWord get() = _currentScrambledWord
+    val score get() = _score
+
+    //Fifth
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
     //Second
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
     }
-
-    var score = 0
-    var currentWordCount = 0
-    private var _currentScrambledWord: String = "test"
-
-    val currentScrambledWord get() = _currentScrambledWord
-
-    //Fifth
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
 
     //Third
     override fun onCleared() {
@@ -37,18 +38,32 @@ class GameViewModel : ViewModel() {
             tempWord.shuffle()
         }
 
-        Log.d("TEST", "currentWord: $currentWord, tempWord: ${String(tempWord)}")
-        _currentScrambledWord = String(tempWord)
-        wordsList.add(currentWord)
-//        if (wordsList.contains(currentWord)) {
-//            Log.d("TEST2", _currentScrambledWord)}
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            currentWordCount += 1
+            wordsList.add(currentWord)
+        }
+    }
 
-//        if (wordsList.contains(currentWord)) {
-//            getNextWord()
-//        } else {
-//            _currentScrambledWord = String(tempWord)
-//            currentWordCount += 1
-//            wordsList.add(currentWord)
-//        }
+    //Seventh
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    //Tenth
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserCorrect(playerWord: String): Boolean {
+        return if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            true
+        } else false
     }
 }
